a957b6186bd8b7af785a21f12574846490b77681
Sunidhi-Pradhan <sunidhipradhan16@gmail.com>
2025-11-12 23:28:52 +0530

    feat: add about page view template

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+
+
 //routes
 router.get('',(req,res) => {
-    res.render('index');
+    const locals ={
+        title : "NodeJS Blog",
+        description : "Simple Blog created with NodeJS, EXpress & MongoDB."
+    }
+    res.render('index',{ locals });
+});
+
+router.get('/about',(req,res) => {
+    res.render('about');
 });
 
 module.exports = router;    
